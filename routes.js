6e8c3deff082f446355251cb8f343df0a7c5492f
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-08-12 15:10:19 +1200

    Current stock data and historical data

--- routes.js
+++ routes.js
@@ -3,12 +3,20 @@ const router = express.Router()
 const utils = require('./utils')
 
 router.get('/', function (req, res) {
-  // utils.readFile('./data.json', req, res, 'home')
-  res.render('home')
+  utils.readFile('./data.json', req, res, 'home')
 })
 
 router.post('/', function (req, res) {
-  utils.search(req, res, 'results')
+  utils.searchCurrent(req, res, 'currentResults')
+  // res.send(req.body)
+})
+
+router.get('/historical', function (req, res) {
+  utils.readFile('./data.json', req, res, 'historical')
+})
+
+router.post('/historical', function (req, res) {
+  utils.searchHistorical(req, res, 'results')
   // res.send(req.body)
 })
 
--- utils.js
+++ utils.js
@@ -32,20 +32,41 @@ function readDetails(fileName, req, res, template) {
   })
 }
 
-function search(req, res, template) {
+function searchCurrent(req, res, template) {
   // Split the search at ' ' and ',' to get an array of symbols
-  const symbols = req.body.search.split(/[ ,]+/)
+  const currentSearchSymbols = req.body.currentSearch.split(/[ ,]+/)
+
+  // Get the stock data for the symbols
+  yahooFinance
+    .quote({
+      symbols: currentSearchSymbols,
+      modules: ['price', 'summaryDetail', 'financialData'],
+    })
+    .then(
+      function (result) {
+        const convertedResult = convertCurrentData(result)
+        res.render(template, convertedResult)
+      },
+      function (err) {
+        return `Whoops, something went wrong!\n${err}`
+      }
+    )
+}
+
+function searchHistorical(req, res, template) {
+  // Split the search at ' ' and ',' to get an array of symbols
+  const historicalSearchSymbols = req.body.historicalSearch.split(/[ ,]+/)
 
   // If currency option is checked
   // Add 'NZDUSD=X' to the the symbols array so we can get the currency conversion rate
-  if (req.body.addCurrency) {
-    symbols.push('NZDUSD=X')
+  if (req.body.addCurrencyHistorical) {
+    historicalSearchSymbols.push('NZDUSD=X')
   }
 
   // Get the stock data for the symbols
   yahooFinance
     .historical({
-      symbols: symbols,
+      symbols: historicalSearchSymbols,
       // Yahoo Finance Historical data filter is off by one day so we need to add 1 day to the date
       from: '2022-03-31',
       to: '2022-04-01',
@@ -53,15 +74,101 @@ function search(req, res, template) {
     })
     .then(
       function (result) {
-        // res.send(result)
         const convertedResult = convertHistoricalData(result)
         res.render(template, convertedResult)
-        // res.send(convertedResult)
       },
       function (err) {
         return `Whoops, something went wrong!\n${err}`
       }
     )
+}
+
+function convertCurrentData(data) {
+  const keys = Object.keys(data)
+
+  console.log(data)
+
+  const reformattedData = {
+    stocks: [],
+  }
+
+  keys.forEach((key) => {
+    // create an empty object to store the stock data
+    const stock = {}
+
+    // If data[key].summaryDetail.totalAssets is defined then it's an ETF
+    if (data[key].summaryDetail.totalAssets) {
+      // Add the summary detail data to the object
+      stock.previousClose = data[key].summaryDetail.previousClose
+      stock.open = data[key].summaryDetail.open
+      stock.high = data[key].summaryDetail.high
+      stock.dayLow = data[key].summaryDetail.dayLow
+      stock.dayHigh = data[key].summaryDetail.dayHigh
+
+      stock.fiftyTwoWeekLow = data[key].summaryDetail.fiftyTwoWeekLow
+      stock.fiftyTwoWeekHigh = data[key].summaryDetail.fiftyTwoWeekHigh
+      stock.volume = data[key].summaryDetail.volume
+      stock.totalAssets = data[key].summaryDetail.totalAssets
+
+      // Add the price data to the stock object
+      stock.shortName = data[key].price.shortName
+      stock.symbol = data[key].price.symbol
+      stock.exchangeName = data[key].price.exchangeName
+      stock.currencySymbol = data[key].price.currencySymbol
+    }
+    // If data[key].summaryDetail.totalAssets is not defined then it's a stock
+    else {
+      // Add the summary detail data to the object
+      stock.previousClose = data[key].summaryDetail.previousClose
+      stock.open = data[key].summaryDetail.open
+      stock.high = data[key].summaryDetail.high
+      stock.dayLow = data[key].summaryDetail.dayLow
+      stock.dayHigh = data[key].summaryDetail.dayHigh
+
+      stock.fiftyTwoWeekLow = data[key].summaryDetail.fiftyTwoWeekLow
+      stock.fiftyTwoWeekHigh = data[key].summaryDetail.fiftyTwoWeekHigh
+      stock.volume = data[key].summaryDetail.volume
+      stock.marketCap = data[key].summaryDetail.marketCap
+
+      stock.trailingPE = data[key].summaryDetail.trailingPE
+      stock.forwardPE = data[key].summaryDetail.forwardPE
+      stock.beta = data[key].summaryDetail.beta
+
+      // Add the price data to the stock object
+      stock.shortName = data[key].price.shortName
+      stock.symbol = data[key].price.symbol
+      stock.exchangeName = data[key].price.exchangeName
+      stock.currencySymbol = data[key].price.currencySymbol
+
+      // Add the financial data to the stock object
+
+      stock.targetLowPrice = data[key].financialData.targetLowPrice
+      stock.targetMeanPrice = data[key].financialData.targetMeanPrice
+      stock.targetHighPrice = data[key].financialData.targetHighPrice
+      stock.targetMedianPrice = data[key].financialData.targetMedianPrice
+      stock.recommendationKey = data[key].financialData.recommendationKey
+      stock.numberOfAnalystOpinions =
+        data[key].financialData.numberOfAnalystOpinions
+
+      stock.totalCash = data[key].financialData.totalCash
+      stock.totalCashPerShare = data[key].financialData.totalCashPerShare
+      stock.totalDebt = data[key].financialData.totalDebt
+      stock.ebitda = data[key].financialData.ebitda
+      stock.totalRevenue = data[key].financialData.totalRevenue
+      stock.grossProfits = data[key].financialData.grossProfits
+      stock.freeCashflow = data[key].financialData.freeCashflow
+      stock.operatingCashflow = data[key].financialData.operatingCashflow
+      stock.earningsGrowth = data[key].financialData.earningsGrowth * 100
+      stock.revenueGrowth = data[key].financialData.revenueGrowth * 100
+      stock.grossMargins = data[key].financialData.grossMargins * 100
+      stock.ebitdaMargins = data[key].financialData.ebitdaMargins * 100
+      stock.operatingMargins = data[key].financialData.operatingMargins * 100
+      stock.profitMargins = data[key].financialData.profitMargins * 100
+    }
+    reformattedData['stocks'].push(stock)
+  })
+
+  return reformattedData
 }
 
 function convertHistoricalData(data) {
@@ -96,6 +203,7 @@ function convertHistoricalData(data) {
 module.exports = {
   readFile,
   readDetails,
-  search,
+  searchCurrent,
+  searchHistorical,
   convertHistoricalData,
 }
